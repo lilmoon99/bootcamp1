93cd8163ceb62393d5df8f307f414a9ca4de1e36
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-30 21:17:42 +0400

    Добавлен простой пример реализации сортировки выбором C#

--- Урок 4. Алгоритм «Сортировка выбором»/SelectionSort/Program.cs
+++ Урок 4. Алгоритм «Сортировка выбором»/SelectionSort/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void SortSelection(int[] collection)
+{
+    int size = collection.Length;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
+    }
+}
+
+int[] array = { 7, 6, 3, 4, 5, 1, 2, 3 };
+System.Console.WriteLine($"{String.Join(',',array)}");
+SortSelection(array);
+System.Console.WriteLine($"{String.Join(',',array)}");
