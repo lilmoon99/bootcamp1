34d07990bcb91843f80bf1f69d55306ed4f099e9
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-30 21:34:28 +0400

    Организована структура проекта, добавлены два вспомогательных модуля

--- Урок 4. Алгоритм «Сортировка выбором»/SelectionSort/Program.cs
+++ Урок 4. Алгоритм «Сортировка выбором»/SelectionSort/Program.cs
@@ -1,21 +1,8 @@
-﻿void SortSelection(int[] collection)
-{
-    int size = collection.Length;
+﻿using static Sortings;
+using static Infrastructure;
 
-    for (int i = 0; i < size - 1; i++)
-    {
-        int pos = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (collection[j] < collection[pos]) pos = j;
-        }
-        int temp = collection[i];
-        collection[i] = collection[pos];
-        collection[pos] = temp;
-    }
-}
+int[] array = CreateArray(10);
 
-int[] array = { 7, 6, 3, 4, 5, 1, 2, 3 };
-System.Console.WriteLine($"{String.Join(',',array)}");
+Show(array);
 SortSelection(array);
-System.Console.WriteLine($"{String.Join(',',array)}");
+Show(array);
