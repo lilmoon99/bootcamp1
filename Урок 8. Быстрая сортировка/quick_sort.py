50b124afd153b8b07dc2d6055c7d4b3e0942de58
Aynur Shamsullin <lilmoon99@ya.ru>
2023-07-13 20:07:40 +0400

    quick sort in python

--- Урок 8. Быстрая сортировка/quick_sort.py
+++ Урок 8. Быстрая сортировка/quick_sort.py
@@ -3,4 +3,14 @@ def quick_sort(arr):
         return arr
     else:
         pivot = arr[0]
-        
+
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+
+        return quick_sort(left) + [pivot] + quick_sort(right)
+    
+arr = [3,1,4,1,5,9,2,6,5,3,5]
+
+sorted_arr = quick_sort(arr)
+
+print(sorted_arr)
