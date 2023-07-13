1c7ca056f3ae37a009325945f1f93d623aec71ac
Aynur Shamsullin <lilmoon99@ya.ru>
2023-07-13 20:23:16 +0400

    quick sort in c# added

--- Урок 8. Быстрая сортировка/Program.cs
+++ Урок 8. Быстрая сортировка/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] QuickSort(int[] arr)
+{
+    if (arr.Length <= 1) return arr;
+    else
+    {
+        int pivot = arr[0];
+        int[] left = arr.Skip(1).Where(x => x < pivot).ToArray();
+        int[] right = arr.Skip(1).Where(x => x >= pivot).ToArray();
+        return QuickSort(left).Concat(new int[] { pivot }).Concat(QuickSort(right)).ToArray();
+
+    }
+}
+
+int[] array = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+int[] array_result = QuickSort(array);
+foreach (var item in array_result)
+{
+    System.Console.Write($"{item} ");
+}
